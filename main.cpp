c015f9b3fdb7d673706e422645e620432248e41f
Sebastian Pinocy <spinocy@gmail.com>
2025-01-28 22:52:50 +0100

    Refactor library to use CMake

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#include"include/batch.h"
-#include"include/loss.h"
-#include"include/module.h"
-#include"include/function.h"
-#include"include/linear.h"
-#include"include/sequential.h"
+#include"batch.h"
+#include"loss.h"
+#include"module.h"
+#include"function.h"
+#include"linear.h"
+#include"sequential.h"
 #include<tuple>
-#include<iomanip>
 
 
 double accuracy(Batch& prediction, Batch& target){
